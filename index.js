4e4cd9000655c0acb67d8c694506ff688fb93eed
huangxingxiong <huangxingxiong@quan5.com>
2018-10-18 10:33:53 +0800

    fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 
-AppRegistry.registerComponent('gxjReactNative', () => App);
+AppRegistry.registerComponent('myApp', () => App);
